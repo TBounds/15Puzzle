4d243908ba7cd357aa0c8f5773983501c534bc5b
Tyler James Bounds <t.bounds@ecs323-8.labs.encs>
2017-01-27 14:06:53 -0800

    Completed section 2.2

---
+++ 15Puzzle/15Puzzle/BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  15Puzzle
+//
+//  Created by Tyler James Bounds on 1/27/17.
+//  Copyright © 2017 Tyler James Bounds. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
